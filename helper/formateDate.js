48a70e1ec047df6f965b317d918b23f4412b8b93
aayushYash <aayash2002@gmail.com>
2023-12-19 23:51:36 +0530

    minor changes in notice event index.html formateDate.js and other files

--- helper/formateDate.js
+++ helper/formateDate.js
@@ -1,12 +1,23 @@
-
-
-const months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+const months = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
 
 const formateDate = (unformatedDate) => {
-    const date = unformatedDate.getDate();
-    const month = months[unformatedDate.getMonth()];
-    const year = unformatedDate.getFullYear();
-    return `${date} ${month} ${year}`;
-}
+  const date = unformatedDate.getDate();
+  const month = months[unformatedDate.getMonth()];
+  const year = unformatedDate.getFullYear();
+  return `${date} ${month} ${year}`;
+};
 
-export {formateDate}; 
+export { formateDate };
--- screens/notices.js
+++ screens/notices.js
@@ -1,56 +1,49 @@
 import { noticesData } from "../firebase/firebase-main.js";
-import {formateDate} from "../helper/formateDate.js";
+import { formateDate } from "../helper/formateDate.js";
 
+const mainContainer = document.querySelector("#main-container");
 
+const main = document.createElement("div");
+console.log(main);
 
+noticesData.forEach((notice) => {
+  const noticeDiv = document.createElement("div");
+  const dateDiv = document.createElement("div");
+  const noticeTitle = document.createElement("h4");
+  const noticeBody = document.createElement("p");
 
-    const main = document.querySelector('#main');
-    console.log(main);
-    if(noticesData.length ==0){
-        main.innerHTML ="<div><h1>No Notice Published Yet!</h1></div>"
-    }
-    else{
-        noticesData.forEach((notice) => {
-        const noticeDiv = document.createElement('div');
-        const dateDiv = document.createElement('div');
-        const noticeTitle = document.createElement('h4');
-        const noticeBody = document.createElement('p');
+  noticeDiv.className = "notice-block";
+  dateDiv.className = "notice-date";
 
+  const noticeDate = new Date(notice.uploadtime.seconds * 1000);
+  const formatedDate = formateDate(noticeDate);
 
-        noticeDiv.className= 'notice-block';
-        dateDiv.className = 'notice-date';
-        
-        const noticeDate = new Date(notice.uploadtime.seconds * 1000);
-        const formatedDate = formateDate(noticeDate);
-        
-        
-        dateDiv.innerText = formatedDate;
-        noticeTitle.innerText = notice.title;
-        noticeBody.innerText = notice.content;
+  dateDiv.innerText = formatedDate;
+  noticeTitle.innerText = notice.title;
+  noticeBody.innerText = notice.content;
 
-        const divContainer = document.createElement('div');
-        divContainer.className = 'notice-container';
-        divContainer.append(noticeTitle,noticeBody);
+  const divContainer = document.createElement("div");
+  divContainer.className = "notice-container";
+  divContainer.append(noticeTitle, noticeBody);
 
-        noticeDiv.append(dateDiv,divContainer);
+  noticeDiv.append(dateDiv, divContainer);
 
+  const downloadButton = document.createElement("div");
+  downloadButton.className = "download-notice-btn";
+  if (notice.pdfurl) {
+    downloadButton.classList.add("visible");
+    const a = document.createElement("a");
+    a.innerHTML = "<span><i class='fas fa-download'></i> <br>Attachment</span>";
+    a.href = notice.pdfurl;
+    downloadButton.appendChild(a);
+    console.log(a);
+  }
+  noticeDiv.appendChild(downloadButton);
+  main.appendChild(noticeDiv);
+});
 
-        const downloadButton = document.createElement('div');
-        downloadButton.className = 'download-notice-btn'
-        if(notice.pdfurl){
-            downloadButton.classList.add('visible')
-            const a = document.createElement('a');
-            a.innerHTML = "<span><i class='fas fa-download'></i> <br>Attachment</span>";
-            a.href = notice.pdfurl;
-            downloadButton.appendChild(a);
-            console.log(a);
-        }
-        noticeDiv.appendChild(downloadButton);
-        main.appendChild(noticeDiv);
-    });
-}
+if (noticesData != 0) mainContainer.innerHTML = main.innerHTML;
 
 // document.addEventListener(onload,() => {
-    
 
 // })
