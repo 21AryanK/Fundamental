2f54e508defd89d6c9a6c09541f4f17c7083cfb2
aayushYash <aayash2002@gmail.com>
2023-12-20 19:30:23 +0530

    fixed nav bar for mobile and other issues

--- screens/events.js
+++ screens/events.js
@@ -6,6 +6,44 @@ const eventContainer = document.querySelector(".event-container");
 
 const container = document.createElement('div');
 
+  /**
+   * Mobile nav toggle
+   */
+  const mobileNavToggleBtn = document.querySelector('.mobile-nav-toggle');
+
+  function mobileNavToogle() {
+    document.querySelector('body').classList.toggle('mobile-nav-active');
+    mobileNavToggleBtn.classList.toggle('bi-list');
+    mobileNavToggleBtn.classList.toggle('bi-x');
+  }
+  mobileNavToggleBtn.addEventListener('click', mobileNavToogle);
+
+  /**
+   * Hide mobile nav on same-page/hash links
+   */
+  document.querySelectorAll('#navmenu a').forEach(navmenu => {
+    navmenu.addEventListener('click', () => {
+      if (document.querySelector('.mobile-nav-active')) {
+        mobileNavToogle();
+      }
+    });
+
+  });
+
+  /**
+   * Toggle mobile nav dropdowns
+   */
+  document.querySelectorAll('.navmenu .has-dropdown i').forEach(navmenu => {
+    navmenu.addEventListener('click', function(e) {
+      if (document.querySelector('.mobile-nav-active')) {
+        e.preventDefault();
+        this.parentNode.classList.toggle('active');
+        this.parentNode.nextElementSibling.classList.toggle('dropdown-active');
+        e.stopImmediatePropagation();
+      }
+    });
+  });
+
 eventsData.forEach((event) => {
     const eventEle = document.createElement('div');
     const eventDescription = document.createElement('div');
